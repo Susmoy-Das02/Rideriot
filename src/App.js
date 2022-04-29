3197272eb7f9a50f19497697d8cb01d31e0b5984
Susmoy-Das02 <methundas269@gmail.com>
2022-04-29 17:16:30 +0600

    navbar component creat done

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import { Route, Routes } from 'react-router-dom';
+import './App.css';
+import Blogs from './Pages/Blogs/Blogs';
+import Home from './Pages/Home/Home/Home';
+import Login from './Pages/Login/Login/Login';
+import Register from './Pages/Login/Register/Register';
+import Footer from './Shared/Footer/Footer';
+import Header from './Shared/Header/Header';
+import NotFound from './Shared/NotFound/NotFound';
+
+function App() {
+  return (
+    <div>
+      <Header></Header>
+      <Routes>
+        <Route path = '/' element={<Home></Home>}></Route>
+        <Route path = '/blogs' element={<Blogs></Blogs>}></Route>
+        <Route path='*' element={<NotFound></NotFound>}></Route>
+        <Route path='/login' element={<Login></Login>}></Route>
+        <Route path='/register' element={<Register></Register>}></Route> 
+      </Routes>
+      <Footer></Footer>
+      
+    </div>
+  );
+}
+
+export default App;
