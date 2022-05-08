85993f947dd260fa91beb2e29215faa127c2e1cd
Susmoy-Das02 <methundas269@gmail.com>
2022-05-09 01:51:15 +0600

    item restock and delivered done

--- src/Pages/Home/ItemDetail/ItemDetail.js
+++ src/Pages/Home/ItemDetail/ItemDetail.js
@@ -1,9 +1,11 @@
+import axios from 'axios';
 import React, { useEffect, useState } from 'react';
-import {useParams} from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 
 const ItemDetail = () => {
     const { itemId } = useParams();
     const [item, setItem] = useState({});
+    const [restock, setRestock] = useState('');
 
     useEffect(() => {
         const url = `https://hidden-tor-69572.herokuapp.com/item/${itemId}`;
@@ -12,6 +14,70 @@ const ItemDetail = () => {
             .then(res => res.json())
             .then(data => setItem(data))
     })
+
+    const changeRestock = (e) => {
+        const value = e.target.value
+        if (value < 0) {
+            alert("Negative value not accept");
+        }
+        else {
+            setRestock(value);
+            
+        }
+
+    }
+
+
+
+    const handleRestock = async (id) => {
+        const updateQuantity = { quantity: parseInt(item.quantity) + parseInt(restock)}
+
+
+        try {
+            const { data } = await axios.patch(`https://hidden-tor-69572.herokuapp.com/update/${id}`, JSON.stringify(updateQuantity), { headers: { 'Content-Type': 'application/json' } });
+
+            if (data) {
+                alert("Restock successfully")
+            }
+
+        }
+        catch (error) {
+            console.log(error.message)
+
+        }
+
+    }
+
+
+    const handleDelivered = async (id) => {
+        if (item.quantity > 0) {
+            const updateQuantity = { quantity: parseInt(item.quantity) - 1 }
+
+
+            try {
+                const { data } = await axios.patch(`http://localhost:5000/update/${id}`, JSON.stringify(updateQuantity), { headers: { 'Content-Type': 'application/json' } });
+
+                if (data) {
+                    alert("delivered successfully")
+                }
+
+            }
+            catch (error) {
+                console.log(error.message)
+
+            }
+
+
+
+        }
+
+        else {
+            alert('Please restock item');
+        }
+
+
+
+    }
 
 
     return (
@@ -34,12 +100,12 @@ const ItemDetail = () => {
 
             <div className='text-center' >
 
-                <button className='btn btn-primary'>Delivered</button>
+                <button onClick={() => handleDelivered(item._id)} className='btn btn-primary'>Delivered</button>
             </div>
 
             <div class="input-group mb-3 mt-3">
-                <input type="number" class="form-control" placeholder="Enter number"/>
-                    <button class="btn btn-outline-secondary" type="button" id="button-addon2">Restock</button>
+                <input onChange={(e) => changeRestock(e)} type="number" class="form-control" placeholder="Enter number" />
+                <button onClick={() => handleRestock(item._id)} class="btn btn-outline-secondary" type="button" id="button-addon2">Restock</button>
             </div>
 
 
