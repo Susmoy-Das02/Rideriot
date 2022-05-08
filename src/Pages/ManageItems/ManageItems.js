1109934ae76724fd32ad6bc3e1b9b8be74c9ad60
Susmoy-Das02 <methundas269@gmail.com>
2022-05-08 17:12:38 +0600

    extra setion add 2

--- src/Pages/ManageItems/ManageItems.js
+++ src/Pages/ManageItems/ManageItems.js
@@ -4,34 +4,48 @@ import useItem from '../../hooks/UseItem';
 const ManageItems = () => {
     const [items, setItems] = useItem();
 
-    const handleDelete = id =>{
+    const handleDelete = id => {
         const proceed = window.confirm('Aru you sure?');
-        if(proceed){
+        if (proceed) {
             const url = `http://localhost:5000/item/${id}`;
             fetch(url, {
                 method: 'DELETE'
             })
-            .then(res => res.json())
-            .then(data =>{
-                console.log(data);
-                const remaining = items.filter(item => item._id !== id);
-                setItems(remaining)
-            })
+                .then(res => res.json())
+                .then(data => {
+                    console.log(data);
+                    const remaining = items.filter(item => item._id !== id);
+                    setItems(remaining)
+                })
 
         }
     }
 
     return (
-        <div className='w-50 mx-auto' >
+        <div className='w-50 mx-auto card' style={{ width: "18rem" }} >
             <h2>Manage your item</h2>
             {
-                items.map(item => <div key = {item._id} >
-                    <h5>{item.name} <button onClick={() => handleDelete(item._id) } >X</button> </h5>
+                items.map(item => <div key={item._id} >
 
-                </div> )
+                        <img src={item.img} className="card-img-top" alt="..." />
+                        <div className="card-body">
+                            <h5 className="card-title">{item.name}</h5>
+                            <h6 className="card-title">Price: {item.price}</h6>
+                            <h6 className="card-title">Quantity: {item.quantity}</h6>
+                            <h6 className="card-title">Supplier name: {item.supplier}</h6>
+                            <p className="card-text">{item.description}</p>
+
+                            <button onClick={() => handleDelete(item._id)}>Delate item</button>
+
+
+
+
+                        </div>
+
+                    </div>)
             }
-        </div>
-    );
+                </div>
+                );
 };
 
-export default ManageItems;
+            export default ManageItems;
